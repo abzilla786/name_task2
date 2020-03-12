82234d8cd79b41acdf4467d0a3f2ef1c654682fb
abdullah ayyaz <abdullahayyaz@tcadmins-MacBook-Air.local>
2020-03-12 11:07:16 +0000

    finished task

---
+++ create_user_class.py
@@ -0,0 +1,7 @@
+class create_user_class:
+    def __init__(self, name):
+        self.name = name
+
+    def create_info_file(self):
+        with open(self.name + '.txt', 'a+') as f:
+            f.write('Name: ' + self.name.)
---
+++ main.py
@@ -0,0 +1,14 @@
+from create_user_class import *
+# read the txt file
+# create 10 object from said file
+
+user_list = []
+
+with open('names.txt','r') as f:
+    x = f.readlines()
+    for i in x:
+        user = create_user_class(i.rstrip('\n'))
+        user_list.append(user)
+
+print(user_list[3].name)
+user_list[3].create_info_file()
